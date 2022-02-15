e8b1e344be6f19c660a2f8c72917395721eb1d3c
Mohan-kotak <mohan.kotak@neosoftmail.com>
2022-02-15 17:41:41 +0530

    android aplash

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import {
+  Platform,
   Text,
   View,
 } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './src/redux/store';
 import { LoginScreen } from './src/screen/loginscreen';
+import SplashScreen from 'react-native-splash-screen';
+
 
 const App = () => {
+  useEffect(() => {
+    {Platform.OS === 'ios'?
+    null:
+    SplashScreen.hide();}
+  }, [])
   return (
     <Provider store={store}>
       <View>
