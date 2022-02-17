8f0bc5a0d0705a219487785f68f452778ae37a73
Shaili Mittal <shaili.mittal@neosoftmail.com>
2022-02-17 14:00:36 +0530

    change

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import {
   Platform,
   Text,
@@ -15,9 +15,11 @@ import { FurnitureRequest } from './src/screen/school/FurnitureRequestScreen/fur
 
 const App = () => {
   useEffect(() => {
-    {Platform.OS === 'ios'?
-    null:
-    SplashScreen.hide();}
+    {
+      Platform.OS === 'ios' ?
+        null :
+        SplashScreen.hide();
+    }
   }, [])
   return (
     <Provider store={store}>
